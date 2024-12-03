bfc57048c19cc7fc9752799784e7c07ba0dc984d
Gunnar Þór Magnússon <gunnarm@tern.is>
2024-12-03 09:22:08 +0000

    Solve part 2

--- src/03.py
+++ src/03.py
@@ -6,24 +6,31 @@ from typing import *
 
 
 def parse(fh):
-    re_mul = re.compile(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)")
+    re_mul = re.compile(r"(mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\))")
+
     lines = []
+    do = True
     for line in fh.readlines():
-        pairs = []
-        while m := re_mul.search(line):
-            pairs.append((int(m.group(1)), int(m.group(2))))
-            line = line[m.span()[1] :]
-        lines.append(pairs)
+        ms = re_mul.findall(line)
+        ops = []
+        for m in ms:
+            if m[0] == "do()":
+                do = True
+            elif m[0] == "don't()":
+                do = False
+            else:
+                ops.append((do, int(m[1]), int(m[2])))
+        lines.append(ops)
 
     return lines
 
 
 def part1(data):
-    return sum(sum(a * b for a, b in pairs) for pairs in data)
+    return sum(sum(a * b for _, a, b in ops) for ops in data)
 
 
 def part2(data):
-    return 0
+    return sum(sum(do * a * b for do, a, b in ops) for ops in data)
 
 
 if __name__ == "__main__":
