1b5403c5776b2dfe7aaa870aae975ff6d816d9e0
Gunnar Þór Magnússon <gunnarm@tern.is>
2024-12-05 06:53:00 +0000

    Part 2

--- src/05.py
+++ src/05.py
@@ -19,20 +19,38 @@ def parse(fh):
     return (rules, lines)
 
 
+def in_order(line, rules):
+    return all(tuple(ab) in rules for ab in itertools.combinations(line, 2))
+
+
 def part1(data):
     rules, lines = data
 
     total = 0
-
     for line in lines:
-        if all(tuple(ab) in rules for ab in itertools.combinations(line, 2)):
+        if in_order(line, rules):
             total += line[len(line) // 2]
 
     return total
 
 
 def part2(data):
-    return 0
+    rules, lines = data
+
+    total = 0
+    for line in lines:
+        if in_order(line, rules):
+            continue
+
+        line = list(line)
+        while not in_order(line, rules):
+            for i in range(len(line) - 1):
+                for j in range(i + 1, len(line)):
+                    if (line[i], line[j]) not in rules and (line[j], line[i]) in rules:
+                        line[i], line[j] = line[j], line[i]
+        total += line[len(line) // 2]
+
+    return total
 
 
 if __name__ == "__main__":
