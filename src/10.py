b4af99d73ee2b3d0e45e4f08cb9364739a16effb
Gunnar Þór Magnússon <gunnarm@tern.is>
2024-12-10 09:28:05 +0000

    Refactor day 10

--- src/10.py
+++ src/10.py
@@ -1,6 +1,5 @@
 import argparse
 import collections
-import itertools
 
 
 def parse(fh):
@@ -12,7 +11,7 @@ def parse(fh):
     return data
 
 
-def part1(data):
+def calculate_paths(data):
     starts = {xy for xy, v in data.items() if v == 0}
     paths = {(start,) for start in starts}
     dirs = [
@@ -25,22 +24,22 @@ def part1(data):
     for _ in range(9):
         new_paths = set()
         for path in paths:
-            # print(f"path={path}")
             x, y = path[-1]
             for dx, dy in dirs:
                 neighbor = (x + dx, y + dy)
                 if neighbor not in data:
-                    # print(f"neighbor={neighbor} not in grid")
                     continue
                 elif data[neighbor] != data[(x, y)] + 1:
-                    # print(f"neighbor={neighbor} not accessible")
                     continue
-                # print(f"{path} -> {neighbor}")
                 new_paths.add(path + (neighbor,))
 
-        # print(f"paths={paths}")
-        # print(f"new_paths={new_paths}")
         paths = new_paths
+
+    return paths
+
+
+def part1(data):
+    paths = calculate_paths(data)
 
     scores = collections.defaultdict(set)
     for path in paths:
@@ -50,34 +49,7 @@ def part1(data):
 
 
 def part2(data):
-    starts = {xy for xy, v in data.items() if v == 0}
-    paths = {(start,) for start in starts}
-    dirs = [
-        (1, 0),
-        (-1, 0),
-        (0, 1),
-        (0, -1),
-    ]
-
-    for _ in range(9):
-        new_paths = set()
-        for path in paths:
-            # print(f"path={path}")
-            x, y = path[-1]
-            for dx, dy in dirs:
-                neighbor = (x + dx, y + dy)
-                if neighbor not in data:
-                    # print(f"neighbor={neighbor} not in grid")
-                    continue
-                elif data[neighbor] != data[(x, y)] + 1:
-                    # print(f"neighbor={neighbor} not accessible")
-                    continue
-                # print(f"{path} -> {neighbor}")
-                new_paths.add(path + (neighbor,))
-
-        # print(f"paths={paths}")
-        # print(f"new_paths={new_paths}")
-        paths = new_paths
+    paths = calculate_paths(data)
 
     return len(paths)
 
