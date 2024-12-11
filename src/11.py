8926e311e24fc3760143746f2e92b0e2bc9da575
Gunnar Þór Magnússon <gunnarm@tern.is>
2024-12-11 22:34:05 +0000

    Be more clever for Alexandr

--- src/11.py
+++ src/11.py
@@ -1,5 +1,6 @@
 import argparse
 import copy
+import functools
 import math
 
 
@@ -11,58 +12,23 @@ def num_digits(n):
     return math.floor(math.log10(n)) + 1
 
 
-def part1(data, blink):
-    for i in range(blink):
-        new_data = []
-        for n in data:
-            if n == 0:
-                new_data.append(1)
-                continue
-            d = num_digits(n)
-            if d % 2 == 0:
-                m = 10 ** (d // 2)
-                new_data.append(n // m)
-                new_data.append(n % m)
-            else:
-                new_data.append(n * 2024)
-        data = new_data
-
-    return len(data)
-
-
-def rec(n, blink, cache):
-    if (n, blink) in cache:
-        return cache[(n, blink)]
-
+@functools.cache
+def rec(n, blink):
     while blink:
         if n == 0:
             n = 1
+        elif (d := num_digits(n)) % 2 == 0:
+            m = 10 ** (d // 2)
+            return rec(n // m, blink - 1) + rec(n % m, blink - 1)
         else:
-            d = num_digits(n)
-            if d % 2 == 0:
-                m = 10 ** (d // 2)
-                a = rec(n // m, blink - 1, cache)
-                b = rec(n % m, blink - 1, cache)
-                cache[(n // m, blink - 1)] = a
-                cache[(n % m, blink - 1)] = b
-                return a + b
-            else:
-                n *= 2024
+            n *= 2024
         blink -= 1
 
     return 1
 
 
 def part2(data, repeat):
-    total = 0
-
-    cache = {}
-    for n in data:
-        a = rec(n, repeat, cache)
-        cache[(n, repeat)] = a
-        total += a
-
-    return total
+    return sum(rec(n, repeat) for n in data)
 
 
 if __name__ == "__main__":
@@ -73,5 +39,5 @@ if __name__ == "__main__":
     with open(args.filename) as fh:
         data = parse(fh)
 
-    print(part1(copy.deepcopy(data), 25))
+    print(part2(copy.deepcopy(data), 25))
     print(part2(copy.deepcopy(data), 75))
