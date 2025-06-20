641ef2e43187932f9baeab4c7e49e0722befb8b0
Gunnar Þór Magnússon <gunnarm@tern.is>
2025-06-20 16:31:55 +0000

    Solve part 2, day 3

--- rust/day03/src/main.rs
+++ rust/day03/src/main.rs
@@ -1,14 +1,31 @@
 use std::str::FromStr;
 use regex::Regex;
 
-fn parse(stdin: std::io::Stdin) -> Vec<Vec<(i64, i64)>> {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let mut data: Vec<Vec<(i64, i64)>> = vec![];
+#[derive(Debug)]
+enum Item{
+    Mul(i64, i64),
+    Do,
+    Dont
+}
+
+fn parse(stdin: std::io::Stdin) -> Vec<Vec<Item>> {
+    let re = Regex::new(r"don't|do|mul\((\d+),(\d+)\)").unwrap();
+    let mut data: Vec<Vec<Item>> = vec![];
 
     for line in stdin.lines() {
-        let mut pairs: Vec<(i64, i64)> = vec![];
-        for (_, [a, b]) in re.captures_iter(&line.unwrap()).map(|c| c.extract()) {
-            pairs.push((i64::from_str(a).unwrap(), i64::from_str(b).unwrap()));
+        let mut pairs: Vec<Item> = vec![];
+        for cap in re.captures_iter(&line.unwrap()) {
+            let m = cap.get(0).unwrap().as_str();
+            let item = match m {
+                "do" => Item::Do,
+                "don't" => Item::Dont,
+                _ => {
+                    let a = cap.get(1).unwrap().as_str();
+                    let b = cap.get(2).unwrap().as_str();
+                    Item::Mul(i64::from_str(a).unwrap(), i64::from_str(b).unwrap())
+                },
+            };
+            pairs.push(item)
         }
         data.push(pairs);
     }
@@ -16,9 +33,34 @@ fn parse(stdin: std::io::Stdin) -> Vec<Vec<(i64, i64)>> {
     data
 }
 
-fn part1(data: &Vec<Vec<(i64, i64)>>) -> i64 {
+fn part1(data: &Vec<Vec<Item>>) -> i64 {
     let pairs = data.iter().flatten();
-    pairs.fold(0, |acc, (a, b)| acc + a * b)
+    pairs.fold(0, |acc, item| { 
+        acc + match item{
+            Item::Mul(a, b) => a * b,
+            _ => 0,
+        }
+    })
+}
+
+fn part2(data: &Vec<Vec<Item>>) -> i64 {
+    let mut sum = 0;
+
+    let mut _do = true;
+    let items = data.iter().flatten();
+    for item in items {
+        match item {
+            Item::Do => _do = true,
+            Item::Dont => _do = false,
+            Item::Mul(a, b) => {
+                if _do {
+                    sum += a * b;
+                }
+            },
+        }
+    }
+
+    sum
 }
 
 fn main() {
@@ -26,4 +68,7 @@ fn main() {
 
     let p1 = part1(&data);
     println!("part1={p1}");
+
+    let p2 = part2(&data);
+    println!("part2={p2}");
 }
