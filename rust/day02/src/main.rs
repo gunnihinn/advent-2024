e23ee2da15704f0c7cae4258af132696502bc12a
Gunnar Þór Magnússon <gunnarm@tern.is>
2025-06-20 13:29:33 +0000

    Day 2 in Rust

--- rust/day02/src/main.rs
+++ rust/day02/src/main.rs
@@ -1,4 +1,5 @@
 use std::str::FromStr;
+use std::iter::zip;
 
 fn parse(stdin: std::io::Stdin) -> Vec<Vec<u64>> {
     let mut reports: Vec<Vec<u64>> = vec![];
@@ -10,11 +11,42 @@ fn parse(stdin: std::io::Stdin) -> Vec<Vec<u64>> {
     return reports;
 }
 
-fn part1(reports: Vec<Vec<u64>>) -> u64 {
-    return 0
+fn safe(report: &Vec<u64>) -> bool {
+    let inc = zip(report.iter(), report.iter().skip(1))
+        .filter(|(a,b)| a <= b )
+        .count();
+    let monotone = inc == 0 || inc == report.len() - 1;
+
+    monotone && 
+    zip(report.iter(), report.iter().skip(1))
+        .all(|(a,b)| 1 <= a.abs_diff(*b) && a.abs_diff(*b) <= 3)
+}
+
+fn part1(reports: Vec<Vec<u64>>) -> usize {
+    reports.iter().filter(|rep| {
+        safe(rep)
+    }).count()
+}
+
+fn tolerate(report: &Vec<u64>) -> bool {
+    report.iter().enumerate().map(|(i, _)| {
+        let tmp: Vec<u64> = report.iter().enumerate().filter(|(j, _)| *j != i).map(|(_, val)| *val).collect();
+        safe(&tmp)
+    }).any(|x| x)
+}
+
+fn part2(reports: Vec<Vec<u64>>) -> usize {
+    reports.iter().filter(|rep| {
+        tolerate(rep)
+    }).count()
 }
 
 fn main() {
     let data = parse(std::io::stdin());
-    println!("data: {data:#?}")
+
+    let res1 = part1(data.clone());
+    println!("part1: {res1:#?}");
+
+    let res2 = part2(data);
+    println!("part2: {res2:#?}");
 }
